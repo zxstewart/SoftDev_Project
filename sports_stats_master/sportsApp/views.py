f2219cfd9d3bbd4d8b3ffef51d7f1b906d2d09ef
davidtdayan <davidtdayan@gmail.com>
2020-09-20 17:27:53 -0600

    added home landing functions in views.py

--- sports_stats_master/sportsApp/views.py
+++ sports_stats_master/sportsApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#function that handles the traffic from the host page of the SportsApp
+def home(request):
+    return HttpResponse('<h1>Sports App Home</h1>')
