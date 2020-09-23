011efd2c717de9dfd08851af82168a32a2c32b77
davidtdayan <davidtdayan@gmail.com>
2020-09-23 01:09:46 -0600

    deleted django project switching to flask

--- sports_stats_master/sportsApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SportsappConfig(AppConfig):
-    name = 'sportsApp'
--- sports_stats_master/sportsApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#function that handles the traffic from the host page of the SportsApp
-def home(request):
-    return HttpResponse('<h1>Sports App Home</h1>')
