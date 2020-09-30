1eac21a014206e180a65e1861f9b3f5e7d574e5b
David Dayan <davdayan@Davids-XPS13.localdomain>
2020-09-30 17:43:22 -0600

    added new testing

--- project-code/testVisualizations.py
+++ project-code/testVisualizations.py
@@ -1,13 +1,18 @@
 #using mpld3 library to create interactive visualizations for the user
 import mpld3
-#import pandas as pd
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sportsreference.nfl.boxscore import Boxscore
+from sportsreference.nfl.teams import Teams
 
 game_data = Boxscore('201912220den')
 print("The Denver Broncos scored: {} points".format(game_data.home_points))
 print("The Detroit Lions scored: {} points".format(game_data.away_points))
+
+#printing to explore what the NFL Teams contains
+for team in Teams():
+    print (team.name, team.wins, team.losses)
 
 df = game_data.dataframe
 print(df.head(20))
