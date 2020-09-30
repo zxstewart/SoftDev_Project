ebd857156f5998a9f51716f4f7a7b528ce44f302
davidtdayan <davidtdayan@gmail.com>
2020-09-30 16:03:24 -0600

    adding templates to support passing python info from DB

--- project-code/sportsApp.py
+++ project-code/sportsApp.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+#using a list of dictionaries on local to just POC of passing dynamic content that will be eventually tied to database
+information = [
+    {
+        'name': 'Peyton Manning',
+        'position': 'Quarterback',
+        'age': '44'
+    },
+    {
+        'name': 'Jamal Murray',
+        'position': 'Point Guard',
+        'age': '23'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, World!'
+    #can pass information (this would be from database calls eventually)
+    return render_template('index.html', posts=information)
 
 #about page
 @app.route('/about')
