59103383e21000274e6750ef69530b11a7bcf8cc
davidtdayan <davidtdayan@gmail.com>
2020-09-30 17:21:44 -0600

    updated packages and testing dynamic visualizations

--- project-code/testVisualizations.py
+++ project-code/testVisualizations.py
@@ -1,3 +1,8 @@
+#using mpld3 library to create interactive visualizations for the user
+import mpld3
+#import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sportsreference.nfl.boxscore import Boxscore
 
 game_data = Boxscore('201912220den')
