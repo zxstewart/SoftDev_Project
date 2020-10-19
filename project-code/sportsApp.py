bfa6c2b087bda17aee4edc21ace58dd6ba8d2349
Zoe Stewart <zost6915@colorado.edu>
2020-10-18 18:36:35 -0600

    have to commit before i can pull i guess

--- project-code/sportsApp.py
+++ project-code/sportsApp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+#from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY']=''
