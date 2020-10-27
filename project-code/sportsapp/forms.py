2140b96b403c1945098b9e0e7c8432a15d3b3d9e
davidtdayan <davidtdayan@gmail.com>
2020-10-27 17:38:59 -0600

    updated registration forms backend to prevent accounts with same username/email from being created and added to db

--- project-code/sportsapp/forms.py
+++ project-code/sportsapp/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 #make sure to also install email_validator: 'pipenv install email_validator'
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from sportsapp.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])   
@@ -11,6 +12,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     interests = StringField('Interests')
     submit = SubmitField('Sign Up')
+
+    #adding custom validation to prevent users with duplicate usernames from being created
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken! Please choose a different one.')
+    
+    #adding custom validation to prevent users with duplicate emails from being created
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is taken or previously used! Please choose a different one.')
 
 class LoginForm(FlaskForm):
     #login uses email over username
