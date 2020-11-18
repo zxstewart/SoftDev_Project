375a6960619889673b173ce93922b8e9913c4ab8
Zoe Stewart <zost6915@colorado.edu>
2020-11-18 16:56:45 -0700

    forgot to update route for settings page

--- project-code/sportsapp/routes.py
+++ project-code/sportsapp/routes.py
@@ -328,6 +328,20 @@ def account():
     image_file = url_for('static', filename='profileImages/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file = image_file, form = form)
 
-@app.route('/settings')
+@app.route('/settings', methods=['GET','POST'])
 def settings():
-    return render_template('settings.html')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profileImages/' + current_user.image_file)
+    return render_template('settings.html', title='Settings', image_file = image_file, form = form)
