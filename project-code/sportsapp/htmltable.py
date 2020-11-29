45a55a3ae660f2702679bfe3f1fae9ede5c4416b
quinnstone <60612999+quinnstone@users.noreply.github.com>
2020-11-28 19:46:53 -0700

    Update htmltable.py

--- project-code/sportsapp/htmltable.py
+++ project-code/sportsapp/htmltable.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 from prettytable import PrettyTable
   
 csv_file = open('/Users/quinnstone/downloads/NFL_stats/Basic_Stats.csv', 'r')
@@ -6,12 +6,15 @@ csv_file = csv_file.readlines()
 line_1 = csv_file[0]
 line_1 = line_1.split(',')
 
-x = PrettyTable([line_1[8],line_1[9],line_1[2],line_1[4],line_1[5],line_1[10],line_1[3],line_1[12]])
+x = PrettyTable([line_1[8],line_1[9],line_1[2],line_1[4],line_1[5],line_1[10],line_1[3]])
 
 for a in range(1,17173):
     line = csv_file[a]
     line = line.split(',')
-    x.add_row([line[8],line[9],line[2],line[4],line[5],line[10],line[3],line[12]])
+    if (line[3] == 'Active'):
+        x.add_row([line[8],line[9],line[2],line[4],line[5],line[10],line[3]])
+    else:
+        pass
 
 html_code = x.get_html_string()
 html_file = open('/Users/quinnstone/Documents/203_4_F20/project-code/sportsapp/templates/Table.html', 'w')
