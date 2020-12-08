ce9b94e577dc4c6f32240fb6e73e9d15e4e20ec6
davidtdayan <davidtdayan@gmail.com>
2020-12-07 20:04:11 -0700

    fixed issues for testing code: database configurations

--- project-code/runSportsApp.py
+++ project-code/runSportsApp.py
@@ -2,4 +2,4 @@ from sportsapp import app
 #another way to run the application without using terminal
 #simply run in terminal with python: $python3 sportsApp.py
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- project-code/sportsapp/__init__.py
+++ project-code/sportsapp/__init__.py
@@ -6,6 +6,11 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+#some generated list of 16 hex values (used for passwords and validation)
+app.config['SECRET_KEY']='0819780287c1fe01cfb39284c1c55b7d'
+#creating database on file system for now using sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 #initialize the imports
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
