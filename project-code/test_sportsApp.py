4678c2dbf839d8d2f644da2d7ef3080cdffd80f7
Zoe Stewart <zost6915@colorado.edu>
2020-12-07 21:05:01 -0700

    updating unittest errors

--- project-code/test_sportsApp.py
+++ project-code/test_sportsApp.py
@@ -1,5 +1,20 @@
-import pytest
-from sportsapp import app
+import pytest, tempfile, os, sportsapp
+from sportsapp import app, db
+from sportsapp.models import User, sportsStats, teamTable, Favorite
+
+
+@pytest.fixture
+def client():
+    db_fd, sportsapp.app.config['DATABASE'] = tempfile.mkstemp()
+    sportsapp.app.config['TESTING'] = True
+
+    with sportsapp.app.test_client() as client:
+        with sportsapp.app.app_context():
+            sportsapp.__init__()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(sportsapp.app.config['DATABASE'])
 
 
 def login(client, username, password):
@@ -15,24 +30,23 @@ def logout(client):
 
 def test_login_logout(client):
     #Make sure login and logout works
+    test = login(client, sportsapp.app.config['USERNAME'], sportsapp.app.config['PASSWORD'])
+    assert b'You were logged in' in test.data
 
-    user = login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'])
-    assert b'You were logged in' in user.data
+    test = logout(client)
+    assert b'You were logged out' in test.data
 
-    user = logout(client)
-    assert b'You were logged out' in user.data
+    test = login(client, sportsapp.app.config['USERNAME'] + 'x', sportsapp.app.config['PASSWORD'])
+    assert b'Invalid username' in test.data
 
-    user = login(client, flaskr.app.config['USERNAME'] + 'x', flaskr.app.config['PASSWORD'])
-    assert b'Invalid username' in user.data
-
-    user = login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'] + 'x')
-    assert b'Invalid password' in user.data
+    test = login(client, sportsapp.app.config['USERNAME'], sportsapp.app.config['PASSWORD'] + 'x')
+    assert b'Invalid password' in test.data
 
 def test_update_profile(client):
     #test user changing profile photo
     login(client, sportsapp.app.config['USERNAME'], sportsapp.app.config['PASSWORD'])
-    user = client.post('/settings', data=dict(
+    test = client.post('/settings', data=dict(
         image_file='sportsapp/static/profileImages/fef05f17b42b8799.png',
     ), follow_redirects=True)
-    assert b'No entries here so far' not in user.data
-    assert b'<strong>HTML</strong> allowed here' in user.data
+    assert b'No entries here so far' not in test.data
+    assert b'<strong>HTML</strong> allowed here' in test.data
